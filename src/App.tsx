83b02945c1bdfa139cd8890c2e909569236b6605
Emmauzishakes <mwangangie284@gmail.com>
2026-03-08 23:23:44 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center justify-center h-screen'>
+        <h1 className='text-4xl font-bold'>Welcome to Inua360</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
